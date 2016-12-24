93169fb3a20e05e8f865652b7cfeee296cf9be5f
Brian Sorahan <bsorahan@gmail.com>
2016-12-24 10:47:57 -0600

    update vendor

--- vendor/github.com/scgolang/exec/cmd_group.go
+++ vendor/github.com/scgolang/exec/cmd_group.go
@@ -2,81 +2,105 @@ package exec
 
 import (
 	"context"
+	"io"
+	"strings"
+	"sync"
 
 	"github.com/pkg/errors"
-	"golang.org/x/sync/errgroup"
 )
 
-// CmdGroup runs a set of commands.
-type CmdGroup struct {
-	addChan   chan *Cmd
-	closeChan chan struct{}
+// Group runs a set of commands.
+type Group struct {
 	cmds      map[string]*Cmd
+	cmdsMutex sync.RWMutex
 	ctx       context.Context
-	group     *errgroup.Group
 }
 
-// NewCmdGroup creates a new CmdGroup instance.
-func NewCmdGroup(ctx context.Context) *CmdGroup {
-	g, gctx := errgroup.WithContext(ctx)
-
-	cg := &CmdGroup{
-		addChan:   make(chan *Cmd),
-		closeChan: make(chan struct{}),
-		cmds:      map[string]*Cmd{},
-		ctx:       gctx,
-		group:     g,
+// NewGroup creates a new Group instance.
+func NewGroup(ctx context.Context) *Group {
+	return &Group{
+		cmds: map[string]*Cmd{},
+		ctx:  ctx,
 	}
-
-	cg.Go(cg.Main)
-
-	return cg
 }
 
-// Add adds a named Cmd to a CmdGroup.
-func (cg *CmdGroup) Add(name string, cmd *Cmd) error {
+// Add adds a named Cmd to a Group.
+func (cg *Group) Add(name, command string, args ...string) error {
+	return cg.AddCmd(name, CommandContext(cg.ctx, command, args...))
+}
+
+// AddCmd adds the provided command to the group.
+func (cg *Group) AddCmd(name string, cmd *Cmd) error {
+	cg.cmdsMutex.Lock()
+	if _, ok := cg.cmds[name]; ok {
+		cg.cmdsMutex.Unlock()
+		return errors.New("command already exists: " + name)
+	}
+	cg.cmds[name] = nil
+	cg.cmdsMutex.Unlock()
+
 	cmd.Name = name
+
+	// Get the stderr pipe.
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		return errors.Wrap(err, "getting stderr pipe")
+	}
+	cmd.stderrPipe = stderr
+
+	// Get the stdout pipe.
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
 		return errors.Wrap(err, "getting stdout pipe")
 	}
 	cmd.stdoutPipe = stdout
+
+	// Start the process and wait for it in a new goroutine.
 	if err := cmd.Start(); err != nil {
 		return errors.Wrap(err, "starting command")
 	}
-	cg.Go(cmd.Wait)
-	cg.addChan <- cmd
+
+	// Add it to the map.
+	cg.cmdsMutex.Lock()
+	cg.cmds[name] = cmd
+	cg.cmdsMutex.Unlock()
+
 	return nil
 }
 
-// Close closes the CmdGroup.
-func (cg *CmdGroup) Close() error {
-	close(cg.closeChan)
-	return nil
+// Output returns the stdout and stderr pipes for the named process.
+func (cg *Group) Output(name string) (stdout, stderr io.ReadCloser, err error) {
+	var (
+		cmd    *Cmd
+		exists bool
+	)
+	cg.cmdsMutex.RLock()
+	cmd, exists = cg.cmds[name]
+	cg.cmdsMutex.RUnlock()
+
+	if !exists {
+		return nil, nil, errors.New("process does not exist: " + name)
+	}
+	return cmd.stdoutPipe, cmd.stderrPipe, nil
 }
 
-// Go runs a new goroutine as part of an errgroup.Group
-func (cg *CmdGroup) Go(f func() error) {
-	cg.group.Go(f)
-}
+// Wait waits for all commands to finish.
+func (cg *Group) Wait() error {
+	cmds := []*Cmd{}
+	cg.cmdsMutex.RLock()
+	for _, cmd := range cg.cmds {
+		cmds = append(cmds, cmd)
+	}
+	cg.cmdsMutex.RUnlock()
 
-// Main runs the background goroutine that manages the CmdGroup
-func (cg *CmdGroup) Main() error {
-ForLoop:
-	for {
-		select {
-		case cmd := <-cg.addChan:
-			cg.cmds[cmd.Name] = cmd
-		case <-cg.closeChan:
-			close(cg.addChan)
-			break ForLoop
+	errs := []string{}
+	for _, cmd := range cmds {
+		if err := cmd.Wait(); err != nil {
+			errs = append(errs, err.Error())
 		}
 	}
-	return nil
-}
-
-// Wait waits for all commands to finish with an exit code of zero,
-// or one to finish with non-zero exit code, whichever happens first.
-func (cg *CmdGroup) Wait() error {
-	return cg.group.Wait()
+	if len(errs) == 0 {
+		return nil
+	}
+	return errors.New(strings.Join(errs, " and "))
 }
--- vendor/github.com/scgolang/exec/cmd_group_test.go
+++ vendor/github.com/scgolang/exec/cmd_group_test.go
@@ -2,32 +2,26 @@ package exec
 
 import (
 	"context"
+	"io/ioutil"
 	"testing"
 )
 
-func TestCmdGroupAddCmd(t *testing.T) {
-	var (
-		group = NewCmdGroup(context.Background())
-		echo  = Command("echo", "foo")
-	)
-	defer group.Close()
-
-	if err := group.Add("echo", echo); err != nil {
+func TestGroupAdd(t *testing.T) {
+	group := NewGroup(context.Background())
+	if err := group.Add("proc1", "echo", "foo"); err != nil {
 		t.Fatal(err)
 	}
 }
 
-func TestCmdGroupAddCmdFailStdoutAlreadySet(t *testing.T) {
+func TestGroupAddCmdFailStdoutAlreadySet(t *testing.T) {
 	var (
-		group = NewCmdGroup(context.Background())
+		group = NewGroup(context.Background())
 		echo  = Command("echo", "foo")
 	)
-	defer group.Close()
-
 	if _, err := echo.StdoutPipe(); err != nil {
 		t.Fatal(err)
 	}
-	if err := group.Add("echo", echo); err == nil {
+	if err := group.AddCmd("echo", echo); err == nil {
 		t.Fatal("expected an error, got nil")
 	} else {
 		if expected, got := `getting stdout pipe: exec: Stdout already set`, err.Error(); expected != got {
@@ -36,14 +30,30 @@ func TestCmdGroupAddCmdFailStdoutAlreadySet(t *testing.T) {
 	}
 }
 
-func TestCmdGroupAddCmdFailLookupError(t *testing.T) {
+func TestGroupAddCmdFailStderrAlreadySet(t *testing.T) {
 	var (
-		group = NewCmdGroup(context.Background())
+		group = NewGroup(context.Background())
+		echo  = Command("echo", "foo")
+	)
+
+	if _, err := echo.StderrPipe(); err != nil {
+		t.Fatal(err)
+	}
+	if err := group.AddCmd("echo", echo); err == nil {
+		t.Fatal("expected an error, got nil")
+	} else {
+		if expected, got := `getting stderr pipe: exec: Stderr already set`, err.Error(); expected != got {
+			t.Fatalf("expected %s, got %s", expected, got)
+		}
+	}
+}
+
+func TestGroupAddCmdFailLookupError(t *testing.T) {
+	var (
+		group = NewGroup(context.Background())
 		echo  = Command("echolalialalialalia")
 	)
-	defer group.Close()
-
-	if err := group.Add("echolalialalialalia", echo); err == nil {
+	if err := group.AddCmd("echolalialalialalia", echo); err == nil {
 		t.Fatal("expected an error, got nil")
 	} else {
 		if expected, got := `starting command: exec: "echolalialalialalia": executable file not found in $PATH`, err.Error(); expected != got {
@@ -52,16 +62,71 @@ func TestCmdGroupAddCmdFailLookupError(t *testing.T) {
 	}
 }
 
-func TestCmdGroupWait(t *testing.T) {
+func TestGroupOutputMissingProcess(t *testing.T) {
+	group := NewGroup(context.Background())
+	_, _, err := group.Output("foo")
+	if err == nil {
+		t.Fatal("expected error, got nil")
+	}
+	if expected, got := `process does not exist: foo`, err.Error(); expected != got {
+		t.Fatalf("expected %s, got %s", expected, got)
+	}
+}
+
+func TestGroupProcessExists(t *testing.T) {
 	var (
-		group   = NewCmdGroup(context.Background())
+		group = NewGroup(context.Background())
+		echo  = Command("echo", "foo")
+	)
+	if err := group.AddCmd("echo", echo); err != nil {
+		t.Fatal(err)
+	}
+	if err := group.AddCmd("echo", echo); err == nil {
+		t.Fatal("expected error, got nil")
+	} else {
+		if expected, got := "command already exists: echo", err.Error(); expected != got {
+			t.Fatalf("expected %s, got %s", expected, got)
+		}
+	}
+}
+
+func TestGroupOutput(t *testing.T) {
+	var (
+		group = NewGroup(context.Background())
+		echo  = Command("echo", "foo")
+	)
+	if err := group.AddCmd("echo", echo); err != nil {
+		t.Fatal(err)
+	}
+	stdout, stderr, err := group.Output("echo")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Read all stdout.
+	output, err := ioutil.ReadAll(stdout)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Read all stderr.
+	if _, err := ioutil.ReadAll(stderr); err != nil {
+		t.Fatal(err)
+	}
+	if err := group.Wait(); err != nil {
+		t.Fatal("expected error, got nil")
+	}
+	if expected, got := "foo\x0A", string(output); expected != got {
+		t.Fatalf("expected %q, got %q", expected, got)
+	}
+}
+
+func TestGroupWait(t *testing.T) {
+	var (
+		group   = NewGroup(context.Background())
 		nobueno = Command("false")
 	)
 
-	if err := group.Add("false", nobueno); err != nil {
-		t.Fatal(err)
-	}
-	if err := group.Close(); err != nil {
+	if err := group.AddCmd("false", nobueno); err != nil {
 		t.Fatal(err)
 	}
 	if err := group.Wait(); err == nil {
